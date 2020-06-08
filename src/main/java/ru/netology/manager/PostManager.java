c776ae0bba12efa393121147ba1bee12e12cde5f
Ksenia Kozyreva <oestara91@gmail.com>
2020-06-08 20:04:00 +0300

    manager added

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    public Post[] posts;
+
+    public Post[] wallSearch(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void wallDelete (int ownerId, int postId) {
+    }
+}
